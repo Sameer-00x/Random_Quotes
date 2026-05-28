23aa57f2f763da661f0cdb261295b14164c10a8d
Sameer-00x <sameer.comix08@gmail.com>
2026-05-28 07:52:35 +0530

    Enter buton Functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const api_url = "https://motivational-spark-api.vercel.app/api/quotes/random";
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
+let btn = document.getElementById("btn");
 
 async function getquote(url) {
     const response = await fetch(url);
@@ -11,6 +12,10 @@ async function getquote(url) {
 
 }
 
-
+document.addEventListener("keydown" ,(event) =>{
+    if(event.key === "Enter"){
+        getquote(api_url);
+    }
+});
 getquote(api_url);
 
